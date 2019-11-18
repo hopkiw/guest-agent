587369107a645b0aaa3a4ce74ba341d7a3166306
Liam Hopkins <liamh@google.com>
2019-11-18 15:59:09 -0800

    use json.Number

--- google_guest_agent/metadata.go
+++ google_guest_agent/metadata.go
@@ -98,7 +98,7 @@ type networkInterfaces struct {
 type project struct {
 	Attributes       attributes
 	ProjectID        string
-	NumericProjectID string
+	NumericProjectID json.Number
 }
 
 type attributes struct {
